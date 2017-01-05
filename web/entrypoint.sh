22aa8478ad4847c67cddf1ca34c1e5fa250fb3de
Italo Maia <italo.maia@gmail.com>
2017-01-04 23:11:44 -0300

    adding ux.location adn ux-dev.location

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -3,5 +3,7 @@
 set -e
 
 envsubst < /tmp/default.raw > /etc/nginx/conf.d/default.conf
+envsubst < /tmp/ux.location > /etc/nginx/ux.location
+envsubst < /tmp/ux-dev.location > /etc/nginx/ux-dev.location
 
 exec "$@"
